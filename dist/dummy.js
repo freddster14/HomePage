ff2daa76db0b5a3e1fe2b5aa95a1ee5f6eaa5ddc
Zeronicc <zeronic4465@gmail.com>
2024-08-27 01:45:56 -0500

    Add img and css to dist, also  compressed images

---
+++ dist/dummy.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// extracted by mini-css-extract-plugin
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZHVtbXkuanMiLCJtYXBwaW5ncyI6Ijs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9Ib21lUGFnZS8uL3NyYy9zdHlsZS5jc3M/ZTMyMCJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,16 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
-
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: 'production',
-  entry: {},
+  entry:  './src/style.css',
   devtool: 'inline-source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    filename: 'dummy.js',
   },
   module: {
     rules: [
@@ -21,7 +22,6 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif|wav|mp3)$/i,
         type: 'asset/resource',
-       
       }
     ]
   },
@@ -30,7 +30,15 @@ module.exports = {
         template: './src/index.html',
         inject: false,
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "main.css"
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/img", to: "images" },
+       
+      ],
+    }), 
   ],
 
   optimization: {
