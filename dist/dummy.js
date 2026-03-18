7912297f513bb87b470c086df82df6a294e64ecc
freddster14 <freddycarrillo951@gmail.com>
2026-03-17 22:01:39 -0500

    update package dependencies and remove unused images; add devicon for tech stack display

--- dist/dummy.js
+++ dist/dummy.js
@@ -1,8 +1,7 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-var __webpack_exports__ = {};
 // extracted by mini-css-extract-plugin
 
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZHVtbXkuanMiLCJtYXBwaW5ncyI6Ijs7O0FBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9Ib21lUGFnZS8uL3NyYy9zdHlsZS5jc3M/ZTMyMCJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBleHRyYWN0ZWQgYnkgbWluaS1jc3MtZXh0cmFjdC1wbHVnaW5cbmV4cG9ydCB7fTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZHVtbXkuanMiLCJtYXBwaW5ncyI6Ijs7QUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL0hvbWVQYWdlLy4vc3JjL3N0eWxlLmNzcz9lMzIwIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIGV4dHJhY3RlZCBieSBtaW5pLWNzcy1leHRyYWN0LXBsdWdpblxuZXhwb3J0IHt9OyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
